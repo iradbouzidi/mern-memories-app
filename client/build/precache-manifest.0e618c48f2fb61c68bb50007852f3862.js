1ef9684b2adf144e563c7651a6a0361906d4eb8d
iradbouzidi <irad.bouzidi@esprit.tn>
2021-01-21 20:45:51 +0100

    done

---
+++ client/build/precache-manifest.0e618c48f2fb61c68bb50007852f3862.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb7aecedca5e5995487a5de62fc1bf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61a1fe72739db44db52c",
+    "url": "/static/css/main.42acf6b3.chunk.css"
+  },
+  {
+    "revision": "79c981e3dea845fb9166",
+    "url": "/static/js/2.94326077.chunk.js"
+  },
+  {
+    "revision": "6f001b22a9cb1fcb0d750f6c56d216df",
+    "url": "/static/js/2.94326077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a1fe72739db44db52c",
+    "url": "/static/js/main.ffda5a61.chunk.js"
+  },
+  {
+    "revision": "7c56bb22be3d35e2fc69",
+    "url": "/static/js/runtime-main.353fc45a.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
