07fdd3e82ab5b9debda8c1aa4dea9245ebeb9643
iradbouzidi <irad.bouzidi@esprit.tn>
2021-01-21 20:37:57 +0100

    done

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import posts from "./posts.js";
+import posts from "./Posts.js";
 import auth from "./auth.js";
 
 export const reducers = combineReducers({ posts, auth });
