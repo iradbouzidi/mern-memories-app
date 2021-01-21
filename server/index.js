1b5fcb6b04496ecc5b4658aa12bdceaddcbf5ffe
iradbouzidi <irad.bouzidi@esprit.tn>
2021-01-21 17:28:10 +0100

    done

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ import cors from "cors";
 import postRoutes from "./routes/posts.js";
 import userRouter from "./routes/user.js";
 
+import dotenv from "dotenv";
+
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,17 +19,20 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL =
-  "mongodb+srv://mern-memories-app:root@mern-memories-app.jtvk3.mongodb.net/mern-memories-app";
+app.get("/", (req, res) => {
+  res.send("Memories API");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   )
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(error.message));
 
 mongoose.set("useFindAndModify", false);
