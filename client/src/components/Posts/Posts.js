3700f6df99a25281fba6c428aef6fdedf22f3cff
iradbouzidi <irad.bouzidi@esprit.tn>
2021-01-21 21:24:47 +0100

    update

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import { Grid, CircularProgress } from '@material-ui/core';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Grid, CircularProgress, Paper, Typography } from "@material-ui/core";
+import { useSelector } from "react-redux";
 
-import Post from './Post/Post';
-import useStyles from './styles';
+import Post from "./Post/Post";
+import useStyles from "./styles";
 
 const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   const classes = useStyles();
 
-  return (
-    !posts.length ? <CircularProgress /> : (
-      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+          Please Sign In to create your own memories and like other's memories.
+        </Typography>
+      </Paper>
+    );
+  } else {
+    return !posts.length ? (
+      <CircularProgress />
+    ) : (
+      <Grid
+        className={classes.container}
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
         {posts.map((post) => (
           <Grid key={post._id} item xs={12} sm={6} md={6}>
             <Post post={post} setCurrentId={setCurrentId} />
           </Grid>
         ))}
       </Grid>
-    )
-  );
+    );
+  }
 };
 
 export default Posts;
