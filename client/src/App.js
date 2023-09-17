1e176efe581eb0e168841fe478df6a42bcd58035
iradbouzidi <irad.bouzidi@esprit.tn>
2023-09-17 18:21:35 +0100

    full post crud

--- client/src/App.js
+++ client/src/App.js
@@ -29,6 +29,7 @@ const App = () => {
         <Container>
           <Grid
             container
+            className={classes.mainContainer}
             justifyContent="space-between"
             alignItems="stretch"
             spacing={3}
--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 //import userRouter from "./routes/user.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -15,12 +16,15 @@ app.use(cors());
 app.use("/posts", postRoutes);
 //app.use("/user", userRouter);
 
-const CONNECTION_URL =
-  "mongodb+srv://irad:irad@training-cluster.p1ezege.mongodb.net/mern-memories-app";
+//const CONNECTION_URL =
+//"mongodb+srv://irad:irad@training-cluster.p1ezege.mongodb.net/mern-memories-app";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
